f3799f7cebd1288e612b5a0abb8bd5e50b7996ca
Matej Hlozek <xhloze02@vutbr.cz>
2021-11-19 21:02:00 +0100

    Added more fprints for scanner_test.c

--- Tests/scanner_tests.c
+++ Tests/scanner_tests.c
@@ -22,6 +22,7 @@ Dyn_string s;
 Dyn_string z;
 Token token;
 FILE *file;
+unsigned token_cnt = 1; // token counter just because
 
 
 int main(){
@@ -59,48 +60,76 @@ assert(dyn_string_compare(&s, "iahoj")==0);
     fprintf(stdout,"-----------------------------------------------\n");
     fprintf(stdout,"------------ Lexical analysis tests -----------\n");
 
-file = fopen("../IFJ21_codes", "r");
+file = fopen("../IFJ21_codes/test_sc1.ifj21", "r");
 get_token(&token);
+fprintf(stdout, "got token n. %d", token_cnt); token_cnt++;
 assert(token.line==1);
+fprintf(stdout, "line check ok");
 assert(token.start==0);
+fprintf(stdout, "start check ok");
 assert(token.end==4);
+fprintf(stdout, "end check ok");
 assert(token.lenght==5);
+fprintf(stdout, "lenght check ok");
 assert(token.type==KEYWORD);
+fprintf(stdout, "type check ok");
 assert(token.value.keyword==KEYWORD_LOCAL);
+fprintf(stdout, "keyword check ok");
+fprintf(stdout, "token n. %d ok", --token_cnt);
 
 get_token(&token);
+fprintf(stdout, "got token n. %d", token_cnt); token_cnt++;
 assert(token.line==1);
+fprintf(stdout, "line check ok");
 assert(token.start==6);
+fprintf(stdout, "start check ok");
 assert(token.end==6);
+fprintf(stdout, "end check ok");
 assert(token.lenght==1);
+fprintf(stdout, "lenght check ok");
 assert(token.type==ID);
+fprintf(stdout, "type check ok");
 //assert(strcmp((token.value.string),"x")==0);
+fprintf(stdout, "token n. %d ok", --token_cnt);
 
 get_token(&token);
+fprintf(stdout, "got token n. %d", token_cnt); token_cnt++;
 assert(token.line==1);
 assert(token.start==7);
 assert(token.end==7);
 assert(token.lenght==1);
 assert(token.type==COLON);
+fprintf(stdout, "token n. %d ok", --token_cnt);
 
 get_token(&token);
+fprintf(stdout, "got token n. %d", token_cnt); token_cnt++;
 assert(token.line==1);
 assert(token.start==9);
 assert(token.end==14);
 assert(token.lenght==6);
 assert(token.type==KEYWORD);
 assert(token.value.keyword==KEYWORD_NUMBER);
+fprintf(stdout, "token n. %d ok", --token_cnt);
 
 get_token(&token);
+fprintf(stdout, "got token n. %d", token_cnt); 
 assert(token.type==ASSIGN);
+fprintf(stdout, "token n. %d ok", token_cnt); token_cnt++;
 get_token(&token);
+fprintf(stdout, "got token n. %d", token_cnt);
 assert(token.type==NUMBER);
 assert(token.value.decimal_value==0.5);
+fprintf(stdout, "token n. %d ok", token_cnt);token_cnt++;
 get_token(&token);
+fprintf(stdout, "got token n. %d", token_cnt);
 assert(token.type==EOL);
+fprintf(stdout, "token n. %d ok", token_cnt); token_cnt++;
 get_token(&token);
+fprintf(stdout, "got token n. %d", token_cnt);
 assert(token.type==STATE_EOF);
+fprintf(stdout, "token n. %d ok", token_cnt); token_cnt++;
 //?assert(token.type==ERROR);
+fclose(file);
 
 
 
