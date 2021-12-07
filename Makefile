35d25fe1cc60d4413c5e4558ac8ea3a5a5be9726
xkoren10 <xkoren10@stud.fit.vutbr.cz>
2021-12-07 22:53:35 +0100

    Ta nevim uš

--- Makefile
+++ Makefile
@@ -6,13 +6,13 @@ CC=gcc
 make: *.c *.h
 	$(CC) $(CFLAGS) *.c *.h -o compiler
 
-test_par: Tests/parser_tests.c dyn_string.c scanner.c symtable.c parser.c parser.h expressions.c stack.c
-	$(CC) $(CFLAGS) Tests/parser_tests.c dyn_string.c scanner.c parser.c symtable.c parser.h expressions.c stack.c -o parser_tests
+test_par: Tests/parser_tests.c dyn_string.c scanner.c symtable.c parser.c parser.h expressions.c code_gen.c stack.c
+	$(CC) $(CFLAGS) Tests/parser_tests.c dyn_string.c scanner.c code_gen.c  parser.c symtable.c parser.h expressions.c stack.c -o parser_tests
 	./parser_tests
 	rm -f parser_tests
 
-test_exp: Tests/expressions_tests.c dyn_string.c scanner.c symtable.c  expressions.c stack.c
-	$(CC) $(CFLAGS) Tests/expressions_tests.c dyn_string.c scanner.c  symtable.c  expressions.c stack.c -o expressions_tests
+test_exp: Tests/expressions_tests.c dyn_string.c scanner.c symtable.c  expressions.c stack.c code_gen.c
+	$(CC) $(CFLAGS) Tests/expressions_tests.c code_gen.c dyn_string.c scanner.c  symtable.c  expressions.c stack.c -o expressions_tests
 	./expressions_tests
 	rm -f expressions_tests
 
--- expressions.c
+++ expressions.c
@@ -32,7 +32,7 @@ bool exp_reduced = false;
 bool list_read;
 
 int expression_analysis(Token *token, ht_table_t *symtable_ptr, Symbol *ret_sym, token_list_t *l)
-{
+{ 
     list_read = false;
     list = l;
     if (list != NULL)
@@ -114,7 +114,6 @@ int analysis()
     case '>':
         if (expression_stack.top->handle == true) //E -> id
         {
-
             expression_stack.top->terminal = false;
             expression_stack.top->handle = false;
             if (expression_stack.top->token.type == INT)
@@ -134,6 +133,11 @@ int analysis()
             }
             else if (expression_stack.top->token.type == ID)
             {
+                ht_item_t *var = ht_search(symtable, expression_stack.top->token.value.string.string);
+                if (var == NULL)
+                {
+                    return ERROR_SEMANTIC;
+                }
                 return_symbol.value_type = "id";
                 return_symbol.id = expression_stack.top->token.value.string.string;
             }
@@ -168,6 +172,9 @@ int analysis()
                 return ERROR_SYNTAX_ANALYSIS;
             }
             output = reduce(el1, el2, el3); //E -> E ? E  alebo E -> (E)
+            if(output != ERROR_OK){
+                return output;
+            }
 
             if ((expression_stack.top->terminal == false) && (expression_stack.top->next->token.type == DOLLAR) && (i2 == 7))
             {
